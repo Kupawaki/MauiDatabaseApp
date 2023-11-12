459c42f025bb6c228e06085e8f6c16867c0ff9ef
Kooper Young <61201332+Kupawaki@users.noreply.github.com>
2023-11-12 00:00:08 -0600

    Initial commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiDatabaseApp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiDatabaseApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
